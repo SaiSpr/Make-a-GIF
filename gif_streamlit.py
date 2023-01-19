a4d06008d27adf71aca46176e081a9d2567cd709
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-01-19 22:25:10 +0530

    Update gif_streamlit.py

--- gif_streamlit.py
+++ gif_streamlit.py
@@ -52,81 +52,81 @@ if uploaded_file is not None:
 	st.session_state.clip_width = clip.w
 	st.session_state.clip_height = clip.h
 	st.session_state.clip_duration = clip.duration
-  	st.session_state.clip_total_frames = clip.duration * clip.fps
-  	st.session_state.clip_fps = st.sidebar.slider('FPS', 10, 60, 20)
+	st.session_state.clip_total_frames = clip.duration * clip.fps
+	st.session_state.clip_fps = st.sidebar.slider('FPS', 10, 60, 20)
 
 	## Display output ##
-  	st.subheader('Metrics')
-  	col1, col2, col3, col4, col5 = st.columns(5)
-  	col1.metric('Width', st.session_state.clip_width, 'pixels')
-  	col2.metric('Height', st.session_state.clip_height, 'pixels')
-  	col3.metric('Duration', st.session_state.clip_duration, 'seconds')
-  	col4.metric('FPS', st.session_state.clip_fps, '')
-  	col5.metric('Total Frames', st.session_state.clip_total_frames, 'frames')
+	st.subheader('Metrics')
+	col1, col2, col3, col4, col5 = st.columns(5)
+	col1.metric('Width', st.session_state.clip_width, 'pixels')
+	col2.metric('Height', st.session_state.clip_height, 'pixels')
+	col3.metric('Duration', st.session_state.clip_duration, 'seconds')
+	col4.metric('FPS', st.session_state.clip_fps, '')
+	col5.metric('Total Frames', st.session_state.clip_total_frames, 'frames')
 
 
-  	# Extract video frame as a display image
-  	st.subheader('Preview')
+	# Extract video frame as a display image
+	st.subheader('Preview')
 
-  	with st.expander('Show image'):
-    		selected_frame = st.slider('Preview a time frame (s)', 0, int(st.session_state.clip_duration), int(np.median(st.session_state.clip_duration)) )
-    		clip.save_frame('frame.gif', t=selected_frame)
-    		frame_image = Image.open('frame.gif')
-    		st.image(frame_image)
+	with st.expander('Show image'):
+		selected_frame = st.slider('Preview a time frame (s)', 0, int(st.session_state.clip_duration), int(np.median(st.session_state.clip_duration)) )
+		clip.save_frame('frame.gif', t=selected_frame)
+		frame_image = Image.open('frame.gif')
+		st.image(frame_image)
 
-  	## Print image parameters ##
-  	st.subheader('Image parameters')
-  	with st.expander('Show image parameters'):
-    		st.write(f'File name: `{uploaded_file.name}`')
-    		st.write('Image size:', frame_image.size)
-    		st.write('Video resolution scaling', selected_resolution_scaling)
-    		st.write('Speed playback:', selected_speedx)
-    		st.write('Export duration:', selected_export_range)
-    		st.write('Frames per second (FPS):', st.session_state.clip_fps)
+	## Print image parameters ##
+	st.subheader('Image parameters')
+	with st.expander('Show image parameters'):
+		st.write(f'File name: `{uploaded_file.name}`')
+		st.write('Image size:', frame_image.size)
+		st.write('Video resolution scaling', selected_resolution_scaling)
+		st.write('Speed playback:', selected_speedx)
+		st.write('Export duration:', selected_export_range)
+		st.write('Frames per second (FPS):', st.session_state.clip_fps)
     
-  	## Export animated GIF ##
-  	st.subheader('Generate GIF')
-  	generate_gif = st.button('Generate Animated GIF')
-  
-  	if generate_gif:
-    		clip = clip.subclip(selected_export_range[0], selected_export_range[1]).speedx(selected_speedx)
-    
-    		frames = []
-    		for frame in clip.iter_frames():
-        		frames.append(np.array(frame))
-    
-    		image_list = []
+	## Export animated GIF ##
+	st.subheader('Generate GIF')
+	generate_gif = st.button('Generate Animated GIF')
+	
+	if generate_gif:
+		clip = clip.subclip(selected_export_range[0], selected_export_range[1]).speedx(selected_speedx)
+		
+		frames = []
+		for frame in clip.iter_frames():
+			frames.append(np.array(frame))
 
-    		for frame in frames:
-        		im = Image.fromarray(frame)
-        		image_list.append(im)
+		image_list = []
+		
+		for frame in frames:
+			im = Image.fromarray(frame)
+			image_list.append(im)
 
-    		image_list[0].save('export.gif', format = 'GIF', save_all = True, loop = 0, append_images = image_list)
-    
+		image_list[0].save('export.gif', format = 'GIF', save_all = True, loop = 0, append_images = image_list)
+
+
  
-    
-    		## Download ##
-    		st.subheader('Download')
-    
-    
-    		file_ = open('export.gif', 'rb')
-    		contents = file_.read()
-    		data_url = base64.b64encode(contents).decode("utf-8")
-    		file_.close()
-    		st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',unsafe_allow_html=True,)
-    
-    		fsize = round(os.path.getsize('export.gif')/(1024*1024), 1)
-    		st.info(f'File size of generated GIF: {fsize} MB', icon='💾')
-    
-    		fname = uploaded_file.name.split('.')[0]
-    		with open('export.gif', 'rb') as file:
-      			btn = st.download_button(
-            			label='Download image',
-            			data=file,
-            			file_name=f'{fname}_scaling-{selected_resolution_scaling}_fps-{st.session_state.clip_fps}_speed-{selected_speedx}_duration-{selected_export_range[0]}-{selected_export_range[1]}.gif',
-            			mime='image/gif'
-          			)
+		## Download ##
+		st.subheader('Download')
 
-## Default page ##
+
+		file_ = open('export.gif', 'rb')
+		contents = file_.read()
+		data_url = base64.b64encode(contents).decode("utf-8")
+		file_.close()
+		st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',unsafe_allow_html=True,)
+
+		fsize = round(os.path.getsize('export.gif')/(1024*1024), 1)
+		st.info(f'File size of generated GIF: {fsize} MB', icon='💾')
+		
+		fname = uploaded_file.name.split('.')[0]
+		with open('export.gif', 'rb') as file:
+			btn = st.download_button(
+				label='Download image',
+				data=file,
+				file_name=f'{fname}_scaling-{selected_resolution_scaling}_fps-{st.session_state.clip_fps}_speed-{selected_speedx}_duration-{selected_export_range[0]}-{selected_export_range[1]}.gif',
+				mime='image/gif'
+				)
+
+## Default page
 else:
 	st.warning('👈 Upload a video file')
